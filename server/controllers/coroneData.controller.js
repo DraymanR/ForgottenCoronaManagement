4a1deca7d32c1c15bb432abf01dff6d20f9a1af8
DraymanR <dr0548424205@gmail.com>
2024-11-14 14:35:18 +0200

    14.11.2024 1

--- server/controllers/coroneData.controller.js
+++ server/controllers/coroneData.controller.js
@@ -1,4 +1,4 @@
-const { connect } = require("../index");
+const { connect } = require("../models/index");
 const { CoronaDataModel } = require("../models/coronaData.model")
 
 module.exports = class CoronaDataController {
